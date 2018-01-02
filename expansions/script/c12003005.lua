36e75822c4e307c921bc94d20aa0bba95d1dc4f6
gg123gg <32418512+gg123gg@users.noreply.github.com>
2018-01-02 13:37:18 +0800

    Update c12003005.lua

--- expansions/script/c12003005.lua
+++ expansions/script/c12003005.lua
@@ -42,7 +42,6 @@ function c12003005.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
 		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 		local e3=Effect.CreateEffect(c)
 		e3:SetType(EFFECT_TYPE_SINGLE)
