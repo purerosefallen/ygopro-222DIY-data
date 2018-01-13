a6c2563df5705f2fbd003047cef4ef7cf225ad99
purerosefallen <78877@qq.com>
2018-01-13 11:59:36 +0800

    fix

--- expansions/script/c50000508.lua
+++ expansions/script/c50000508.lua
@@ -1,100 +1,100 @@
 --灵装之星宿骑士
 function c50000508.initial_effect(c)
-	--synchro summon
-	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsSetCard,0x50e),1)
-	c:EnableReviveLimit()
-	--return to deck
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(50000508,0))
-	e1:SetCategory(CATEGORY_TODECK)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(c50000508.tdcon)
-	e1:SetTarget(c50000508.tdtg)
-	e1:SetOperation(c50000508.tdop)
-	--to hand
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(50000508,2))
-	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_LEAVE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,50000508)
-	e2:SetCondition(c50000508.thcon)
-	e2:SetTarget(c50000508.thtg)
-	e2:SetOperation(c50000508.thop)
-	c:RegisterEffect(e2)
-	--neg
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(50000508,1))
-	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_CHAINING)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(c50000508.con)
-	e3:SetTarget(c50000508.tg)
-	e3:SetOperation(c50000508.op)
-	c:RegisterEffect(e3)
+    --synchro summon
+    aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsSetCard,0x50e),1)
+    c:EnableReviveLimit()
+    --return to deck
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(50000508,0))
+    e1:SetCategory(CATEGORY_TODECK)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition(c50000508.tdcon)
+    e1:SetTarget(c50000508.tdtg)
+    e1:SetOperation(c50000508.tdop)
+    --to hand
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(50000508,2))
+    e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_LEAVE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,50000508)
+    e2:SetCondition(c50000508.thcon)
+    e2:SetTarget(c50000508.thtg)
+    e2:SetOperation(c50000508.thop)
+    c:RegisterEffect(e2)
+    --neg
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(50000508,1))
+    e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_CHAINING)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetCondition(c50000508.con)
+    e3:SetTarget(c50000508.tg)
+    e3:SetOperation(c50000508.op)
+    c:RegisterEffect(e3)
 end
 function c50000508.tdcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 function c50000508.tdfilter(c)
-	return c:IsAbleToDeck()
+    return c:IsAbleToDeck()
 end
 function c50000508.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and c50000508.tdfilter(chkc) end
-	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+    if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and c50000508.tdfilter(chkc) end
+    if chk==0 then return true end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
 end
 function c50000508.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsControler(1-tp) then
-		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
-	end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and tc:IsControler(1-tp) then
+        Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
+    end
 end
 function c50000508.con(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=re:GetHandler()
-	return re:IsActiveType(TYPE_MONSTER) and rc~=c and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
+    local c=e:GetHandler()
+    local rc=re:GetHandler()
+    return re:IsActiveType(TYPE_MONSTER) and rc~=c and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
 function c50000508.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
-	end
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+    end
 end
 function c50000508.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=re:GetHandler()
-	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
-	   Duel.Destroy(rc,REASON_EFFECT)
-	end
+    local c=e:GetHandler()
+    local rc=re:GetHandler()
+    if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
+       Duel.Destroy(rc,REASON_EFFECT)
+    end
 end
 function c50000508.thcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return (c:IsReason(REASON_BATTLE) or (c:GetReasonPlayer()~=tp and c:IsReason(REASON_EFFECT)))
-		and c:IsPreviousPosition(POS_FACEUP)
+    local c=e:GetHandler()
+    return (c:IsReason(REASON_BATTLE) or (c:GetReasonPlayer()~=tp and c:IsReason(REASON_EFFECT)))
+        and c:IsPreviousPosition(POS_FACEUP)
 end
 function c50000508.thfilter(c)
-	return c:IsSetCard(0x50e) and c:IsAbleToHand()
+    return c:IsSetCard(0x50e) and c:IsAbleToHand()
 end
 function c50000508.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c50000508.thfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_DECK)
+    if chk==0 then return Duel.IsExistingMatchingCard(c50000508.thfilter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_DECK)
 end
 function c50000508.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c50000508.thfilter),tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-		Duel.ShuffleDeck(tp)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c50000508.thfilter),tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil)
+    if g:GetCount()>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+        Duel.ShuffleDeck(tp)
+    end
 end
