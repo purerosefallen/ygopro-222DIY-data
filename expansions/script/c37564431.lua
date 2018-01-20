f794bdf37ba5f86f0ecad96da67e3d416aebb05b
purerosefallen <78877@qq.com>
2018-01-20 20:33:29 +0800

    fix

--- expansions/script/c37564431.lua
+++ expansions/script/c37564431.lua
@@ -52,7 +52,7 @@ function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
-	local g1=Duel.SelectMatchingCard(tp,cm.filter1,tp,LOCATION_HAND,0,1,1,nil)
+	local g1=Duel.SelectMatchingCard(tp,cm.filter1,tp,LOCATION_HAND,0,1,1,nil,tp)
 	if g1:GetCount()>0 then
 		Duel.ConfirmCards(1-tp,g1)
 		local tc=g1:GetFirst()
