631b9e7fcb75aec299fa798225ec8fba46127c15
purerosefallen <78877@qq.com>
2018-01-29 01:11:13 +0000

    Auto Push by Nanahira

--- expansions/script/c37564803.lua
+++ expansions/script/c37564803.lua
@@ -6,4 +6,11 @@ cm.fusion_att_3L=ATTRIBUTE_FIRE
 function cm.initial_effect(c)
 	Senya.AddSummonMusic(c,m*16,SUMMON_TYPE_FUSION)
 	Senya.Fusion_3L_Attribute(c,cm)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1000)
+	c:RegisterEffect(e2)	
 end
