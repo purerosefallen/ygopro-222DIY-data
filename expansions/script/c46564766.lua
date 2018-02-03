925522a857442c3b63bf96018301ff8947d92922
purerosefallen <78877@qq.com>
2018-02-03 13:34:08 +0800

    fix

--- expansions/script/c46564766.lua
+++ expansions/script/c46564766.lua
@@ -1,14 +1,14 @@
 --哭 泣 是 让 心 情 去 散 步
-function c46564764.initial_effect(c)
+function c46564766.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(c46564764.fufilter),2,true) 
+	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(c46564766.fufilter),2,true) 
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c46564764.splimit)
+	e1:SetValue(c46564766.splimit)
 	c:RegisterEffect(e1)
 	--special summon rule
 	local e2=Effect.CreateEffect(c)
@@ -16,15 +16,15 @@ function c46564764.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(c46564764.sprcon)
-	e2:SetOperation(c46564764.sprop)
+	e2:SetCondition(c46564766.sprcon)
+	e2:SetOperation(c46564766.sprop)
 	c:RegisterEffect(e2)
 	--level
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetTarget(c46564764.lvtg)
-	e3:SetOperation(c46564764.lvop)
+	e3:SetTarget(c46564766.lvtg)
+	e3:SetOperation(c46564766.lvop)
 	c:RegisterEffect(e3)
 	--tohand
 	local e4=Effect.CreateEffect(c)
@@ -32,51 +32,51 @@ function c46564764.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_BE_MATERIAL)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCondition(c46564764.thcon)
-	e4:SetTarget(c46564764.thtg)
-	e4:SetOperation(c46564764.thop)
+	e4:SetCondition(c46564766.thcon)
+	e4:SetTarget(c46564766.thtg)
+	e4:SetOperation(c46564766.thop)
 	c:RegisterEffect(e4)
 end
-function c46564764.fufilter(c)
+function c46564766.fufilter(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_TUNER)
 end
-function c46564764.splimit(e,se,sp,st)
+function c46564766.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
-function c46564764.rfilter(c,fc)
+function c46564766.rfilter(c,fc)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_TUNER) and c:IsCanBeFusionMaterial(fc)
 end
-function c46564764.spfilter1(c,tp,g)
-	return g:IsExists(c46564764.spfilter2,1,c,tp,c)
+function c46564766.spfilter1(c,tp,g)
+	return g:IsExists(c46564766.spfilter2,1,c,tp,c)
 end
-function c46564764.spfilter2(c,tp,mc)
+function c46564766.spfilter2(c,tp,mc)
 	return Duel.GetLocationCountFromEx(tp,tp,Group.FromCards(c,mc))>0
 end
-function c46564764.sprcon(e,c)
+function c46564766.sprcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local rg=Duel.GetReleaseGroup(tp):Filter(c46564764.rfilter,nil,c)
-	return rg:IsExists(c46564764.spfilter1,1,nil,tp,rg)
+	local rg=Duel.GetReleaseGroup(tp):Filter(c46564766.rfilter,nil,c)
+	return rg:IsExists(c46564766.spfilter1,1,nil,tp,rg)
 end
-function c46564764.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetReleaseGroup(tp):Filter(c46564764.rfilter,nil,c)
+function c46564766.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	local rg=Duel.GetReleaseGroup(tp):Filter(c46564766.rfilter,nil,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g1=rg:FilterSelect(tp,c46564764.spfilter1,1,1,nil,tp,rg)
+	local g1=rg:FilterSelect(tp,c46564766.spfilter1,1,1,nil,tp,rg)
 	local mc=g1:GetFirst()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g2=rg:FilterSelect(tp,c46564764.spfilter2,1,1,mc,tp,mc)
+	local g2=rg:FilterSelect(tp,c46564766.spfilter2,1,1,mc,tp,mc)
 	g1:Merge(g2)
 	c:SetMaterial(g1)
 	Duel.Release(g1,REASON_COST+REASON_FUSION+REASON_MATERIAL)
 end
-function c46564764.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c46564764.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+function c46564766.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c46564766.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_GRAVE,0,1,5,nil)
 	local ct=Duel.Remove(g,POS_FACEUP,REASON_COST)
 	e:SetLabel(ct)
 end
-function c46564764.lvop(e,tp,eg,ep,ev,re,r,rp)
+function c46564766.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -87,19 +87,19 @@ function c46564764.lvop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
-function c46564764.thcon(e,tp,eg,ep,ev,re,r,rp)
+function c46564766.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
 end
-function c46564764.filter(c)
+function c46564766.filter(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
 end
-function c46564764.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c46564764.filter,tp,LOCATION_REMOVED,0,1,nil) end
+function c46564766.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c46564766.filter,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
-function c46564764.thop(e,tp,eg,ep,ev,re,r,rp)
+function c46564766.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c46564764.filter,tp,LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c46564766.filter,tp,LOCATION_REMOVED,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
