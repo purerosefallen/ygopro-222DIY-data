02b2110ec9500a6bfbd99a1668e46abdfd4be112
MobiusMei <mobiusmei233@gmail.com>
2018-02-10 04:11:14 -0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 912807a9d768ca52f4e74e2106ef1b7180152441.

--- expansions/script/c50008008.lua
+++ expansions/script/c50008008.lua
@@ -1,191 +1,96 @@
-<<<<<<< HEAD
 --妖精传说-贝儿
 local m=50008008
 local cm=_G["c"..m]
 function cm.initial_effect(c)
-    --summon
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(m,0))
-    e1:SetCategory(CATEGORY_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetTarget(cm.sumtg)
-    e1:SetOperation(cm.sumop)
-    c:RegisterEffect(e1)
-    --effect gain
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_BE_MATERIAL)
-    e2:SetCondition(cm.efcon)
-    e2:SetOperation(cm.efop)
-    c:RegisterEffect(e2)
+	--summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(cm.smtg)
+	e1:SetOperation(cm.smop)
+	c:RegisterEffect(e1)
+	--effect gain
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCondition(cm.efcon)
+	e2:SetOperation(cm.efop)
+	c:RegisterEffect(e2)
 end
-function cm.filter(c)
-    return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsSummonable(true,nil)
+function cm.filter(c,e,tp)
+	return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function cm.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_HAND,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+function cm.smtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function cm.sumop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-    local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_HAND,0,1,1,nil)
-    if g:GetCount()>0 then
-        Duel.Summon(tp,g:GetFirst(),true,nil)
-    end
-end
-function cm.efcon(e,tp,eg,ep,ev,re,r,rp)
-    return ( r==REASON_LINK or r==REASON_SYNCHRO )
-end
-function cm.efop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local rc=c:GetReasonCard()
-    if not rc:IsRace(RACE_SPELLCASTER) then return end
-    local e1=Effect.CreateEffect(rc)
-    e1:SetDescription(aux.Stringid(m,1))
-    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TODECK)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetCost(cm.cost)
-    e1:SetTarget(cm.tgtg)
-    e1:SetOperation(cm.tgop)
-    e1:SetReset(RESET_EVENT+0x1fe0000)
-    rc:RegisterEffect(e1,true)
-    if not rc:IsType(TYPE_EFFECT) then
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_ADD_TYPE)
-        e2:SetValue(TYPE_EFFECT)
-        e2:SetReset(RESET_EVENT+0x1fe0000)
-        rc:RegisterEffect(e2,true)
-    end
-end
-function cm.cfilter(c)
-    return (c:IsRace(RACE_SPELLCASTER) or c:IsType(TYPE_SPELL)) and c:IsAbleToRemoveAsCost()
-end
-function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,cm.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
+function cm.smop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 
-function cm.tgfilter(c)
-    return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsAbleToGrave()
-end
-function cm.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.tgfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-end
-function cm.tgop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,cm.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if g:GetCount()>0 then
-        if Duel.SendtoGrave(g,REASON_EFFECT)>0 and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(m,2)) then
-            Duel.BreakEffect()
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-            local tg=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,0,LOCATION_GRAVE,1,1,nil)
-            Duel.HintSelection(tg)
-            Duel.SendtoDeck(tg,nil,2,REASON_EFFECT)
-        end
-    end
-end
-=======
---妖精传说-贝儿
-local m=50008008
-local cm=_G["c"..m]
-function cm.initial_effect(c)
-    --summon
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(m,0))
-    e1:SetCategory(CATEGORY_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetTarget(cm.sumtg)
-    e1:SetOperation(cm.sumop)
-    c:RegisterEffect(e1)
-    --effect gain
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e2:SetCode(EVENT_BE_MATERIAL)
-    e2:SetCondition(cm.efcon)
-    e2:SetOperation(cm.efop)
-    c:RegisterEffect(e2)
-end
-function cm.filter(c)
-    return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsSummonable(true,nil)
-end
-function cm.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_HAND,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
-end
-function cm.sumop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-    local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_HAND,0,1,1,nil)
-    if g:GetCount()>0 then
-        Duel.Summon(tp,g:GetFirst(),true,nil)
-    end
-end
 function cm.efcon(e,tp,eg,ep,ev,re,r,rp)
-    return ( r==REASON_LINK or r==REASON_SYNCHRO )
+	return ( r==REASON_LINK or r==REASON_SYNCHRO )
 end
 function cm.efop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local rc=c:GetReasonCard()
-    if not rc:IsRace(RACE_SPELLCASTER) then return end
-    local e1=Effect.CreateEffect(rc)
-    e1:SetDescription(aux.Stringid(m,1))
-    e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TODECK)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetCost(cm.cost)
-    e1:SetTarget(cm.tgtg)
-    e1:SetOperation(cm.tgop)
-    e1:SetReset(RESET_EVENT+0x1fe0000)
-    rc:RegisterEffect(e1,true)
-    if not rc:IsType(TYPE_EFFECT) then
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_ADD_TYPE)
-        e2:SetValue(TYPE_EFFECT)
-        e2:SetReset(RESET_EVENT+0x1fe0000)
-        rc:RegisterEffect(e2,true)
-    end
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	if not rc:IsRace(RACE_SPELLCASTER) then return end
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(m,1))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.tgtg)
+	e1:SetOperation(cm.tgop)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		rc:RegisterEffect(e2,true)
+	end
 end
 function cm.cfilter(c)
-    return (c:IsRace(RACE_SPELLCASTER) or c:IsType(TYPE_SPELL)) and c:IsAbleToRemoveAsCost()
+	return (c:IsRace(RACE_SPELLCASTER) or c:IsType(TYPE_SPELL)) and c:IsAbleToRemoveAsCost()
 end
 function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,cm.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,cm.cfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 
 function cm.filter(c)
-    return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsAbleToGrave()
+	return c:IsRace(RACE_SPELLCASTER) and c:GetAttack()==1850 and c:IsAbleToGrave()
 end
 function cm.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.tgfilter,tp,LOCATION_DECK,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 function cm.tgop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,cm.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if g:GetCount()>0 then
-        if Duel.SendtoGrave(g,REASON_EFFECT)>0 and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_GRAVE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(m,2)) then
-            Duel.BreakEffect()
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-            local tg=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,1,nil)
-            Duel.HintSelection(tg)
-            Duel.SendtoDeck(tg,nil,2,REASON_EFFECT)
-        end
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,cm.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		if Duel.SendtoGrave(g,REASON_EFFECT)>0 and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_GRAVE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(m,2)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+			local tg=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,1,nil)
+			Duel.HintSelection(tg)
+			Duel.SendtoDeck(tg,nil,2,REASON_EFFECT)
+		end
+	end
 end
->>>>>>> origin/master
--- expansions/script/c71400011.lua
+++ expansions/script/c71400011.lua
@@ -9,7 +9,6 @@ function c71400011.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(71400011,0))
-	e1:SetCountLimit(1,71400011)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_MZONE)
