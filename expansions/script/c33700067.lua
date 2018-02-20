9461bedcf4d06bb8c2873fae668ba30a6fff0f19
purerosefallen <78877@qq.com>
2018-02-20 16:14:58 +0800

    fix

--- expansions/script/c33700067.lua
+++ expansions/script/c33700067.lua
@@ -36,8 +36,9 @@ function c33700067.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function c33700067.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local rc=e:GetHandler():GetReasonCard()
-	return e:GetHandler():IsReason(REASON_EFFECT) and e:GetHandler():IsPreviousLocation(LOCATION_DECK) and e:GetHandler():GetPreviousControler()==tp and rc and rc:IsSetCard(0x442) and rc~=e:GetHandler() and Duel.GetMZoneCount(tp)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_RULE) and c:IsPreviousLocation(LOCATION_DECK) and c:GetPreviousControler()==tp and rc and rc:IsSetCard(0x442) and rc~=c and Duel.GetMZoneCount(tp)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c33700067.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetMZoneCount(tp)>0
