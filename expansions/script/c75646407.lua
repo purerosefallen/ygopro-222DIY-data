53867f260d3ab0ea8408a61ae5c68a726af9b1f6
purerosefallen <78877@qq.com>
2018-03-03 11:48:18 +0800

    fix

--- expansions/script/c75646407.lua
+++ expansions/script/c75646407.lua
@@ -56,8 +56,7 @@ end
 function c75646407.sprcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c75646407.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,3,nil)
+	return Duel.IsExistingMatchingCard(c75646407.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,3,nil) and Duel.GetLocationCountFromEx(tp)>0
 end
 function c75646407.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
