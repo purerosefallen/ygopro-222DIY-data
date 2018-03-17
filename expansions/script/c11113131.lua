cf3b4e78e4e1f991310e042492283134d16f83fc
purerosefallen <78877@qq.com>
2018-03-17 18:23:33 +0800

    update data

--- expansions/script/c11113131.lua
+++ expansions/script/c11113131.lua
@@ -7,6 +7,9 @@ function c11113131.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
+		if chk==0 then return c11113131[tp]>0 end
+	end)
 	e1:SetOperation(c11113131.activate)
 	c:RegisterEffect(e1)
 	--summon limit
