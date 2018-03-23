47f04ce768cc934eea297dcf0b051aee8f073d3e
Hidarime_Yume <s.sonic@qq.com>
2018-03-23 20:25:17 +0800

    Update c71400006.lua

--- expansions/script/c71400006.lua
+++ expansions/script/c71400006.lua
@@ -6,6 +6,7 @@ function c71400006.initial_effect(c)
 	--summon success
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(71400006,0))
+	e1:SetCountLimit(1,71400006)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
