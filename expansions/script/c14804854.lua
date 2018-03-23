286d4f634fdc9253261ac862ac243709899cc460
purerosefallen <78877@qq.com>
2018-03-23 19:16:38 +0800

    update data

--- expansions/script/c14804854.lua
+++ expansions/script/c14804854.lua
@@ -2,7 +2,7 @@
 function c14804854.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,14804854+EFFECT_COUNT_CODE_OATH)
