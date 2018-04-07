63bcf0ad59b2d6426c2169d17cfa31c36547846e
purerosefallen <78877@qq.com>
2018-04-07 18:40:59 +0800

    update data

---
+++ expansions/script/c13257222.lua
@@ -0,0 +1,93 @@
+--宇宙军阀首领 邪神红水晶
+function c13257222.initial_effect(c)
+	--double tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DOUBLE_TRIBUTE)
+	e1:SetValue(c13257222.condition)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(13257222,0))
+	e2:SetCategory(CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,13257222)
+	e2:SetTarget(c13257222.target)
+	e2:SetOperation(c13257222.activate)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--cannot be target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	local e12=Effect.CreateEffect(c)
+	e12:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e12:SetCode(EVENT_SUMMON_SUCCESS)
+	e12:SetOperation(c13257222.bgmop)
+	c:RegisterEffect(e12)
+end
+function c13257222.condition(e,c)
+	return c:IsRace(RACE_MACHINE)
+end
+function c13257222.filter(c)
+	return c:IsSetCard(0x353) and c:IsSummonable(true,nil,1)
+end
+function c13257222.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c13257222.filter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function c13257222.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,c13257222.filter,tp,LOCATION_HAND,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		local s1=tc:IsSummonable(true,nil,1)
+		if s1 then
+			Duel.Summon(tp,tc,true,nil,1)
+			local label=tc:GetFlagEffectLabel(13257200)
+			tc:RegisterFlagEffect(13257200,RESET_EVENT+0x1fe0000,EFFECT_FLAG_CLIENT_HINT,1,label+2,aux.Stringid(13257222,5))
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetCode(EVENT_LEAVE_FIELD_P)
+			e1:SetLabelObject(e:GetHandler())
+			e1:SetOperation(c13257222.checkop)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+			e2:SetCode(EVENT_LEAVE_FIELD)
+			e2:SetOperation(c13257222.spop)
+			e2:SetLabelObject(e1)
+			tc:RegisterEffect(e2)
+			Duel.Hint(11,0,aux.Stringid(13257222,6))
+		end
+	end
+end
+function c13257222.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsDisabled() then
+		e:SetLabel(1)
+	else e:SetLabel(0) end
+end
+function c13257222.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject():GetLabel()~=0 then return end
+	local tc=GetLabelObject():GetLabelObject()
+	if tc and tc:IsLocation(LOCATION_GRAVE) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		Duel.Hint(11,0,aux.Stringid(13257222,6))
+	end
+end
+function c13257222.bgmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(11,0,aux.Stringid(13257222,6))
+end
---
+++ expansions/script/c13257223.lua
@@ -0,0 +1,94 @@
+--宇宙战争兵器 主炮 495年的折反镭射
+function c13257223.initial_effect(c)
+	c:EnableReviveLimit()
+	--equip limit
+	local e11=Effect.CreateEffect(c)
+	e11:SetType(EFFECT_TYPE_SINGLE)
+	e11:SetCode(EFFECT_EQUIP_LIMIT)
+	e11:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e11:SetValue(c13257223.eqlimit)
+	c:RegisterEffect(e11)
+	--immune
+	local e12=Effect.CreateEffect(c)
+	e12:SetType(EFFECT_TYPE_SINGLE)
+	e12:SetCode(EFFECT_IMMUNE_EFFECT)
+	e12:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e12:SetRange(LOCATION_SZONE)
+	e12:SetCondition(c13257223.econ)
+	e12:SetValue(c13257223.efilter)
+	c:RegisterEffect(e12)
+	--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(700)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--Negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(13257223,2))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DICE)
+	e3:SetType(EFFECT_TYPE_QUICK_F)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(c13257223.codisable)
+	e3:SetTarget(c13257223.tgdisable)
+	e3:SetOperation(c13257223.opdisable)
+	c:RegisterEffect(e3)
+	c:RegisterFlagEffect(13257201,0,0,0,5)
+	
+end
+function c13257223.eqlimit(e,c)
+	local eg=c:GetEquipGroup()
+	local lv=c:GetOriginalLevel()
+	if lv==nil then lv=0 end
+	if not eg:IsContains(e:GetHandler()) then
+		eg:AddCard(e:GetHandler())
+	end
+	local cl=c:GetFlagEffectLabel(13257200)
+	if cl==nil then
+		cl=0
+	end
+	local er=e:GetHandler():GetFlagEffectLabel(13257201)
+	if er==nil then
+		er=0
+	end
+	return not (er>cl) and not (eg:Filter(Card.IsSetCard,nil,0x354):GetSum(Card.GetLevel)>lv) and not c:GetEquipGroup():IsExists(Card.IsCode,1,e:GetHandler(),e:GetHandler():GetCode())
+end
+function c13257223.econ(e)
+	return e:GetHandler():GetEquipTarget()
+end
+function c13257223.efilter(e,re)
+	return e:GetHandlerPlayer()~=re:GetOwnerPlayer()
+end
+function c13257223.codisable(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp
+		and not e:GetHandler():IsStatus(STATUS_CHAINING)
+end
+function c13257223.tgdisable(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c13257223.opdisable(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or Duel.GetCurrentChain()~=ev+1 or c:IsStatus(STATUS_BATTLE_DESTROYED) then
+		return
+	end
+	local cl=c:GetEquipTarget():GetFlagEffectLabel(13257200)
+	if cl==nil then
+		cl=0
+	end
+	local d=Duel.TossDice(tp,1)
+	if cl<d and Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+		Duel.Damage(1-tp,cl*cl*40,REASON_EFFECT)
+	end
+end
