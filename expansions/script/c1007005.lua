0dce54b01b823b203042538dd020209c1034f12c
篁楠 <HuangYuNan@users.noreply.github.com>
2018-04-15 15:22:55 +0800

    Update c1007005.lua
    fix

--- expansions/script/c1007005.lua
+++ expansions/script/c1007005.lua
@@ -128,7 +128,7 @@ function c1007005.spfilter(c,e,tp)
 	return c:IsSetCard(0x20f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c1007005.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(c1007005.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
