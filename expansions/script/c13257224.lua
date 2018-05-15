a1084fee26a8d4543cd90eae59c52534b1a02c65
Nanahira <78877@qq.com>
2018-05-15 20:30:19 +0800

    Add files via upload

--- expansions/script/c13257224.lua
+++ expansions/script/c13257224.lua
@@ -1,6 +1,6 @@
 --宇宙战争兵器 量产炮 自瞄镭射
 function c13257224.initial_effect(c)
-	c:EnableCounterPermit(0x354)
+	c:EnableCounterPermit(0x354,LOCATION_SZONE)
 	c:SetCounterLimit(0x354,10)
 	c:EnableReviveLimit()
 	--equip limit
