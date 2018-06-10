c3cbc317bc49cdec70127700d3d4654b70177cc0
Nanahira <78877@qq.com>
2018-06-10 11:52:45 +0800

    Add files via upload

--- expansions/script/c22600018.lua
+++ expansions/script/c22600018.lua
@@ -1,93 +1,98 @@
---音语—二重奏之吉他×2
+--音语—桃夭之琴瑟
 function c22600018.initial_effect(c)
-    --link summon
-    c:EnableReviveLimit()
-    aux.AddLinkProcedure(c,nil,2,2,c22600018.spcheck)
-    --remove
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(22600018,0))
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCondition(c22600018.rmcon)
-    e1:SetTarget(c22600018.rmtg)
-    e1:SetOperation(c22600018.rmop)
-    c:RegisterEffect(e1)
-    --remove
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(22600018,1))
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetHintTiming(0,0x1c0)
-    e2:SetCountLimit(1,22600018)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetCategory(CATEGORY_REMOVE+CATEGORY_TOGRAVE)
-    e2:SetTarget(c22600018.retg)
-    e2:SetOperation(c22600018.reop)
-    c:RegisterEffect(e2)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(Card.IsSetCard,0x260),1)
+	c:EnableReviveLimit()
+
+	--remove
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c22600018.rmcon)
+	e1:SetOperation(c22600018.rmop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+
+	--atk and damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(22600018,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c22600018.atkcon)
+	e3:SetOperation(c22600018.atkop)
+	c:RegisterEffect(e3)
+
+	--deck to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCondition(c22600018.dhcon)
+	e4:SetTarget(c22600018.dhtg)
+	e4:SetOperation(c22600018.dhop)
+	c:RegisterEffect(e4)
 end
-function c22600018.spcheck(g)
-    return g:GetClassCount(Card.GetLevel)==g:GetCount() and g:GetClassCount(Card.GetLinkAttribute)==g:GetCount() and g:GetClassCount(Card.GetLinkRace)==1
+
+function c22600018.cfilter(c,g)
+	return c:IsSetCard(0x260) and c:IsControler(tp)
 end
+
 function c22600018.rmcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+	return eg:IsExists(c22600018.cfilter,1,e:GetHandler(),tp)
 end
-function c22600018.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil)
-    if chk==0 then return g:GetCount()>0 end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-end
+
 function c22600018.rmop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
-        if g:GetCount()==0 then return end
-        local rg=g:RandomSelect(tp,1)
-        local tc=rg:GetFirst()
-        Duel.Remove(tc,POS_FACEDOWN,REASON_EFFECT)
-        tc:RegisterFlagEffect(22600018,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e1:SetCode(EVENT_PHASE+PHASE_END)
-        e1:SetCountLimit(1)
-        e1:SetLabelObject(tc)
-        e1:SetReset(RESET_PHASE+PHASE_END)
-        e1:SetCondition(c22600018.retcon)
-        e1:SetOperation(c22600018.retop)
-        Duel.RegisterEffect(e1,tp)
+	local g=Duel.GetDecktopGroup(1-tp,1)
+	Duel.DisableShuffleCheck()
+	Duel.Hint(HINT_CARD,0,22600018)
+	Duel.Remove(g,POS_FACEDOWN,REASON_EFFECT)
 end
-function c22600018.retcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetLabelObject():GetFlagEffect(22600018)~=0
+
+function c22600018.dhcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_SYNCHRO) and bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
 end
-function c22600018.retop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=e:GetLabelObject()
-    Duel.SendtoHand(tc,1-tp,REASON_EFFECT)
+
+function c22600018.filter(c,e,tp)
+	return c:IsSetCard(0x260) and c:IsAbleToHand()
 end
-function c22600018.refilter1(c)
-    return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x260)
+
+function c22600018.dhtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c22600018.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND+CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK)
 end
-function c22600018.refilter2(c)
-    return c:IsType(TYPE_MONSTER)
+
+function c22600018.dhop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c22600018.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
-function c22600018.retg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return false end
-    if chk==0 then return Duel.IsExistingTarget(c22600018.refilter1,tp,LOCATION_MZONE,0,1,nil)
-        and Duel.IsExistingTarget(c22600018.refilter2,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g1=Duel.SelectTarget(tp,c22600018.refilter1,tp,LOCATION_MZONE,0,1,1,nil)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g2=Duel.SelectTarget(tp,c22600018.refilter2,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g1,1,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g2,1,0,0)
+
+function c22600018.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsControler,1,nil,1-tp)
 end
-function c22600018.reop(e,tp,eg,ep,ev,re,r,rp)
-    local ex1,dg=Duel.GetOperationInfo(0,CATEGORY_TOGRAVE)
-    local ex2,cg=Duel.GetOperationInfo(0,CATEGORY_REMOVE)
-    local dc=dg:GetFirst()
-    local cc=cg:GetFirst()
-    if dc:IsRelateToEffect(e) and cc:IsRelateToEffect(e) then
-        Duel.SendtoGrave(dc,REASON_EFFECT)
-        Duel.Remove(cc,POS_FACEDOWN,REASON_EFFECT)
-    end
+
+function c22600018.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=eg:FilterCount(Card.IsControler,nil,1-tp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ct*100)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+	Duel.Hint(HINT_CARD,0,22600018)
+	Duel.Damage(1-tp,ct*100,REASON_EFFECT)
 end
--- expansions/script/c22600024.lua
+++ expansions/script/c22600024.lua
@@ -1,89 +1,93 @@
 --音语—二重奏之吉他×2
-function c22600024.initial_effect(c)
-	--link summon
-	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,nil,2,2,c22600024.spcheck)
-
-	--remove
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(22600024,0))
-	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,22600024)
-	e1:SetCondition(c22600024.rmcon)
-	e1:SetTarget(c22600024.rmtg)
-	e1:SetOperation(c22600024.rmop)
-	c:RegisterEffect(e1)
-
-	--remove
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(22600024,1))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCountLimit(1,22600025)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_TOGRAVE)
-	e2:SetTarget(c22600024.retg)
-	e2:SetOperation(c22600024.reop)
-	c:RegisterEffect(e2)
+function c22600018.initial_effect(c)
+    --link summon
+    c:EnableReviveLimit()
+    aux.AddLinkProcedure(c,nil,2,2,c22600018.spcheck)
+    --remove
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(22600018,0))
+    e1:SetCategory(CATEGORY_REMOVE)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCondition(c22600018.rmcon)
+    e1:SetTarget(c22600018.rmtg)
+    e1:SetOperation(c22600018.rmop)
+    c:RegisterEffect(e1)
+    --remove
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(22600018,1))
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetHintTiming(0,0x1c0)
+    e2:SetCountLimit(1,22600018)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCategory(CATEGORY_REMOVE+CATEGORY_TOGRAVE)
+    e2:SetTarget(c22600018.retg)
+    e2:SetOperation(c22600018.reop)
+    c:RegisterEffect(e2)
 end
-
-function c22600024.spcheck(g)
-	return g:GetClassCount(Card.GetLevel)==g:GetCount() and g:GetClassCount(Card.GetLinkAttribute)==g:GetCount() and g:GetClassCount(Card.GetLinkRace)==1
+function c22600018.spcheck(g)
+    return g:GetClassCount(Card.GetLevel)==g:GetCount() and g:GetClassCount(Card.GetLinkAttribute)==g:GetCount() and g:GetClassCount(Card.GetLinkRace)==1
 end
-
-function c22600024.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+function c22600018.rmcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
-
-function c22600024.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	if chk==0 then return g:GetCount()>0 end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+function c22600018.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil)
+    if chk==0 then return g:GetCount()>0 end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
-
-function c22600024.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local g1=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	local g2=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
-	if g1:GetCount()>0 then 
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		local sg1=g1:Select(tp,1,1,nil)
-		Duel.Remove(sg1:GetFirst(),POS_FACEDOWN,REASON_EFFECT)
-	end
-	if g2:GetCount()>0 then
-		local sg2=g2:RandomSelect(1-tp,1)
-		Duel.Remove(sg2:GetFirst(),POS_FACEDOWN,REASON_EFFECT)
-	end
+function c22600018.rmop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_HAND,nil)
+        if g:GetCount()==0 then return end
+        local rg=g:RandomSelect(tp,1)
+        local tc=rg:GetFirst()
+        Duel.Remove(tc,POS_FACEDOWN,REASON_EFFECT)
+        tc:RegisterFlagEffect(22600018,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e1:SetCode(EVENT_PHASE+PHASE_END)
+        e1:SetCountLimit(1)
+        e1:SetLabelObject(tc)
+        e1:SetReset(RESET_PHASE+PHASE_END)
+        e1:SetCondition(c22600018.retcon)
+        e1:SetOperation(c22600018.retop)
+        Duel.RegisterEffect(e1,tp)
 end
-
-function c22600024.refilter(c)
-	return c:IsType(TYPE_MONSTER)
+function c22600018.retcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetLabelObject():GetFlagEffect(22600018)~=0
 end
-
-function c22600024.retg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then 
-		return Duel.IsExistingTarget(c22600024.refilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(c22600024.refilter,tp,0,LOCATION_MZONE,1,nil) 
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectTarget(tp,c22600024.refilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g2=Duel.SelectTarget(tp,c22600024.refilter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g1,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g2,1,0,0)
+function c22600018.retop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=e:GetLabelObject()
+    Duel.SendtoHand(tc,1-tp,REASON_EFFECT)
 end
-
-function c22600024.reop(e,tp,eg,ep,ev,re,r,rp)
-	local ex1,dg=Duel.GetOperationInfo(0,CATEGORY_TOGRAVE)
-	local ex2,cg=Duel.GetOperationInfo(0,CATEGORY_REMOVE)
-	local dc=dg:GetFirst()
-	local cc=cg:GetFirst()
-	if dc:IsRelateToEffect(e) and cc:IsRelateToEffect(e) then
-		Duel.SendtoGrave(dc,REASON_EFFECT)
-		Duel.Remove(cc,POS_FACEDOWN,REASON_EFFECT)
-	end
+function c22600018.refilter1(c)
+    return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x260)
+end
+function c22600018.refilter2(c)
+    return c:IsType(TYPE_MONSTER)
+end
+function c22600018.retg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return false end
+    if chk==0 then return Duel.IsExistingTarget(c22600018.refilter1,tp,LOCATION_MZONE,0,1,nil)
+        and Duel.IsExistingTarget(c22600018.refilter2,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local g1=Duel.SelectTarget(tp,c22600018.refilter1,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g2=Duel.SelectTarget(tp,c22600018.refilter2,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g1,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g2,1,0,0)
+end
+function c22600018.reop(e,tp,eg,ep,ev,re,r,rp)
+    local ex1,dg=Duel.GetOperationInfo(0,CATEGORY_TOGRAVE)
+    local ex2,cg=Duel.GetOperationInfo(0,CATEGORY_REMOVE)
+    local dc=dg:GetFirst()
+    local cc=cg:GetFirst()
+    if dc:IsRelateToEffect(e) and cc:IsRelateToEffect(e) then
+        Duel.SendtoGrave(dc,REASON_EFFECT)
+        Duel.Remove(cc,POS_FACEDOWN,REASON_EFFECT)
+    end
 end
