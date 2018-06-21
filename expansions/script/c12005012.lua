0f07aa4c7a8e959169e7dd97c3fa80f47b233b6b
gg123gg <32418512+gg123gg@users.noreply.github.com>
2018-06-21 20:11:24 +0800

    Update c12005012.lua

--- expansions/script/c12005012.lua
+++ expansions/script/c12005012.lua
@@ -1,62 +1,99 @@
 --六曜的牙月丘依儿
 function c12005012.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xfba),c12005012.matfilter,true)
-	--SpecialSummon
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(12005012,0))
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetOperation(c12005012.thop)
-	c:RegisterEffect(e1)
-	--tohand
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(12005012,1))
-	e2:SetCategory(CATEGORY_LEAVE_GRAVE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(c12005012.drcost)
-	e2:SetTarget(c12005012.sptg)
-	e2:SetOperation(c12005012.spop)
-	c:RegisterEffect(e2)
+    --fusion material
+    c:EnableReviveLimit()
+    aux.AddFusionProcFun2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xfba),c12005012.matfilter,true)
+    --SpecialSummon
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_EXTRA)
+    e1:SetCondition(c12005012.spcon)
+    e1:SetOperation(c12005012.spop)
+    c:RegisterEffect(e1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(12005012,0))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetOperation(c12005012.thop)
+    c:RegisterEffect(e1)
+    --tohand
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(12005012,1))
+    e2:SetCategory(CATEGORY_LEAVE_GRAVE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCost(c12005012.drcost)
+    e2:SetTarget(c12005012.sptg1)
+    e2:SetOperation(c12005012.spop1)
+    c:RegisterEffect(e2)
+end
+function c12005012.cfilter1(c,tp,g)
+    return c:IsFaceup()
+end
+function c12005012.spfilter(c,fc)
+    return c12005012.matfilter(c) and c:IsCanBeFusionMaterial(fc)
+end
+function c12005012.spfilter1(c,tp,g)
+    return g:IsExists(c12005012.spfilter2,1,c,tp,c)
+end
+function c12005012.spfilter2(c,tp,mc)
+    return Duel.GetLocationCountFromEx(tp,tp,Group.FromCards(c,mc))>0
+end
+function c12005012.spcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    local g=Duel.GetReleaseGroup(tp):Filter(c12005012.spfilter,nil,c)
+    return g:IsExists(c12005012.spfilter1,1,nil,tp,g)
+end
+function c12005012.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=Duel.GetReleaseGroup(tp):Filter(c12005012.spfilter,nil,c)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local g1=g:FilterSelect(tp,c12005012.spfilter1,1,1,nil,tp,g)
+    local mc=g1:GetFirst()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local g2=g:FilterSelect(tp,c12005012.spfilter2,2,2,mc,tp,mc)
+    g1:Merge(g2)
+    c:SetMaterial(g1)
+    Duel.Release(g1,REASON_COST+REASON_FUSION+REASON_MATERIAL)
 end
 function c12005012.matfilter(c)
-	return c:IsSetCard(0xfb0) and c:IsReleasable() and c:IsFaceup()
+    return c:IsSetCard(0xfb0) and c:IsReleasable() and c:IsFaceup()
 end
 function c12005012.thop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_TO_HAND)
-	e1:SetTargetRange(0,LOCATION_DECK)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EFFECT_CANNOT_DRAW)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	e2:SetTargetRange(0,1)
-	Duel.RegisterEffect(e2,tp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_TO_HAND)
+    e1:SetTargetRange(0,LOCATION_DECK)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+    local e2=Effect.CreateEffect(e:GetHandler())
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EFFECT_CANNOT_DRAW)
+    e2:SetReset(RESET_PHASE+PHASE_END)
+    e2:SetTargetRange(0,1)
+    Duel.RegisterEffect(e2,tp)
 end
 function c12005012.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToExtraAsCost() end
-	Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_COST)
+    if chk==0 then return e:GetHandler():IsAbleToExtraAsCost() end
+    Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_COST)
 end
 function c12005012.filter(c)
-	return c:IsSetCard(0xfbb) and c:IsAbleToHand()
+    return c:IsSetCard(0xfbb) and c:IsAbleToHand()
 end
-function c12005012.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:GetControler()==tp and chkc:GetLocation()==LOCATION_GRAVE and c12005012.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c12005012.filter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,c12005012.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+function c12005012.sptg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:GetControler()==tp and chkc:GetLocation()==LOCATION_GRAVE and c12005012.filter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(c12005012.filter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectTarget(tp,c12005012.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
-function c12005012.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-	end
+function c12005012.spop1(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+    end
 end
