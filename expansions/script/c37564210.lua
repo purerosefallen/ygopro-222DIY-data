2ba2c5f31f63460134f5fff6c3cec63909632617
purerosefallen <78877@qq.com>
2018-06-25 02:50:07 +0000

    Auto Push by Nanahira

--- expansions/script/c37564210.lua
+++ expansions/script/c37564210.lua
@@ -2,6 +2,7 @@
 local m=37564210
 local cm=_G["c"..m]
 xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
+cm.Senya_name_with_sawawa=true
 function cm.initial_effect(c)
 	--Senya.setreg(c,m,37564299)
 	c:SetUniqueOnField(1,1,m)
