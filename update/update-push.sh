705efd2bfb1619a2d02555f1bec887eaabc9caf1
purerosefallen <78877@qq.com>
2018-07-08 22:12:31 +0800

    update data

--- update/update-push.sh
+++ update/update-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i "s/\r//g" ../expansions/script/*
+sed -i "s/\r//g" ../expansions/script/*.lua
 sed -i "s/\r//g" ../expansions/*.conf
-sed -i "s/\r//g" ../deck
-mono ./update.exe -ci
+sed -i "s/\r//g" ../deck/*.ydk
+mono update.exe -ci
 git add ../. -A
 git commit -m "update data"
 git push origin master
