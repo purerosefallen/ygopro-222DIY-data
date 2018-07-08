bf9cf464631f741c93295da32c60b48445820e15
purerosefallen <78877@qq.com>
2018-07-08 04:03:14 +0000

    Auto Push by Nanahira

---
+++ expansions/script/c37564347.lua
@@ -0,0 +1,39 @@
+--苍月的忏悔诗
+local m=37564347
+local cm=_G["c"..m]
+xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
+function cm.initial_effect(c)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_EFFECT),3,5)
+	c:EnableReviveLimit()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(m,1))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCountLimit(1,m)
+	e4:SetTarget(cm.destg)
+	e4:SetOperation(cm.desop)
+	c:RegisterEffect(e4)
+	local ex=e4:Clone()
+	ex:SetCode(EVENT_PHASE+PHASE_END)
+	c:RegisterEffect(ex)
+end
+function cm.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function cm.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	if g:GetCount()>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
