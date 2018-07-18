e2740e455317441222bdf89f93bf0aa9c4139480
purerosefallen <78877@qq.com>
2018-07-18 22:55:35 +0800

    update data

--- update/update-push.sh
+++ update/update-push.sh
@@ -2,6 +2,9 @@
 sed -i "s/\r//g" ../expansions/script/*.lua
 sed -i "s/\r//g" ../expansions/*.conf
 sed -i "s/\r//g" ../deck/*.ydk
+rm -rf ../expansions/script/sed*
+rm -rf ../expansions/sed*
+rm -rf ../deck/sed*
 mono update.exe -ci
 git add ../. -A
 git commit -m "update data"
