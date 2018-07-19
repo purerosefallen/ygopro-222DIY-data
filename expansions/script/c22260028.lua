d8446d4c528a2b8bde204fc0ee66e5d88e3c8020
purerosefallen <78877@qq.com>
2018-07-19 20:50:09 +0800

    update data

--- expansions/script/c22260028.lua
+++ expansions/script/c22260028.lua
@@ -2,86 +2,85 @@
 local m=22260028
 local cm=_G["c"..m]
 function cm.initial_effect(c)
-    c:EnableReviveLimit()
-    --special summon
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_SPSUMMON_PROC)
-    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCondition(cm.sprcon)
-    c:RegisterEffect(e1)
-    --to hand
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(m,1))
-    e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1,222600280)
-    e2:SetTarget(cm.thtg)
-    e2:SetOperation(cm.thop)
-    c:RegisterEffect(e2)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(cm.sprcon)
+	c:RegisterEffect(e1)
+	--to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(m,1))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(cm.thtg)
+	e2:SetOperation(cm.thop)
+	c:RegisterEffect(e2)
 end
 function cm.filter(c)
-    return c:IsFaceup() and (c:IsCode(22260027) or c:IsType(TYPE_SPELL))
+	return c:IsFaceup() and (c:IsCode(22260027) or c:IsType(TYPE_SPELL))
 end
 function cm.sprcon(e,c)
-    if c==nil then return true end
-    local tp=c:GetControler()
-    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_ONFIELD,0,1,nil)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_ONFIELD,0,1,nil)
 end
 function cm.thfilter1(c)
-    return c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
 function cm.thfilter2(c)
-    return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
 function cm.thfilter3(c)
-    return c:IsType(TYPE_TRAP) and c:IsAbleToHand()
+	return c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
 function cm.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,1,nil) and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK+LOCATION_EXTRA)>0 end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
-    local ac=Duel.AnnounceCard(tp)
-    Duel.SetTargetParam(ac)
-    Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,1,nil) and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK+LOCATION_EXTRA)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
+	local ac=Duel.AnnounceCard(tp)
+	Duel.SetTargetParam(ac)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function cm.thop(e,tp,eg,ep,ev,re,r,rp)
-    local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-    local g=Duel.GetFieldGroup(tp,0,LOCATION_DECK+LOCATION_EXTRA)
-    if g:GetCount()<1 then return end
-    Duel.ConfirmCards(1-tp,g)
-    Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_CONFIRM)
-    local sg=g:FilterSelect(1-tp,Card.IsCode,1,1,nil,ac)
-    local tc=sg:GetFirst()
-    if tc then
-        Duel.ConfirmCards(tp,sg)
-        if tc:IsType(TYPE_MONSTER) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-            local g=Duel.SelectMatchingCard(tp,cm.thfilter1,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-            if g:GetCount()>0 then
-                Duel.SendtoHand(g,nil,REASON_EFFECT)
-                Duel.ConfirmCards(1-tp,g)
-            end
-        elseif tc:IsType(TYPE_SPELL) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-            local g=Duel.SelectMatchingCard(tp,cm.thfilter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-            if g:GetCount()>0 then
-                Duel.SendtoHand(g,nil,REASON_EFFECT)
-                Duel.ConfirmCards(1-tp,g)
-            end
-        elseif tc:IsType(TYPE_TRAP) then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-            local g=Duel.SelectMatchingCard(tp,cm.thfilter3,tp, LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-            if g:GetCount()>0 then
-                Duel.SendtoHand(g,nil,REASON_EFFECT)
-                Duel.ConfirmCards(1-tp,g)
-            end
-        else
-            return false
-        end
-    end
-    Duel.ShuffleDeck(1-tp)
+	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_DECK+LOCATION_EXTRA)
+	if g:GetCount()<1 then return end
+	Duel.ConfirmCards(1-tp,g)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_CONFIRM)
+	local sg=g:FilterSelect(1-tp,Card.IsCode,1,1,nil,ac)
+	local tc=sg:GetFirst()
+	if tc then
+		Duel.ConfirmCards(tp,sg)
+		if tc:IsType(TYPE_MONSTER) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,cm.thfilter1,tp,LOCATION_DECK,0,1,1,nil)
+			if g:GetCount()>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		elseif tc:IsType(TYPE_SPELL) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,cm.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+			if g:GetCount()>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		elseif tc:IsType(TYPE_TRAP) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,cm.thfilter3,tp, LOCATION_DECK,0,1,1,nil)
+			if g:GetCount()>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		else
+			return false
+		end
+	end
+	Duel.ShuffleDeck(1-tp)
 end
