ce81ab79355aa12439405e8774765ba57fc8e6f1
gg123gg <32418512+gg123gg@users.noreply.github.com>
2018-08-10 21:10:55 +0800

    Update c12010037.lua

--- expansions/script/c12010037.lua
+++ expansions/script/c12010037.lua
@@ -1,105 +1,108 @@
 --靜儀式 青の博物館
 function c12010037.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(c12010037.activate)
-	c:RegisterEffect(e1)
-	--special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(12010037,1))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetRange(LOCATION_FZONE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_TO_HAND)
-	e1:SetTarget(c12010037.sptg)
-	e1:SetOperation(c12010037.spop)
-	c:RegisterEffect(e1)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetOperation(c12010037.activate)
+    c:RegisterEffect(e1)
+    --special summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(12010037,1))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_TO_HAND)
+    e1:SetTarget(c12010037.sptg)
+    e1:SetOperation(c12010037.spop)
+    c:RegisterEffect(e1)
 
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(12010037,2))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetRange(LOCATION_FZONE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_TO_HAND)
-	e1:SetTarget(c12010037.sptg1)
-	e1:SetOperation(c12010037.spop1)
-	c:RegisterEffect(e1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(12010037,2))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_TO_HAND)
+    e1:SetTarget(c12010037.sptg1)
+    e1:SetOperation(c12010037.spop1)
+    c:RegisterEffect(e1)
 end
 function c12010037.thfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xfbb) and c:IsAbleToHand()
+    return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xfbb) and c:IsAbleToHand()
 end
 function c12010037.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(c12010037.thfilter,tp,LOCATION_DECK,0,nil)
-	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(12010037,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,sg)
-	end
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local g=Duel.GetMatchingGroup(c12010037.thfilter,tp,LOCATION_DECK,0,nil)
+    if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(12010037,0)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.SendtoHand(sg,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,sg)
+    end
 end
 function c12010037.filter(c,tp)
-	return (c:IsType(TYPE_TRAP+TYPE_CONTINUOUS) or c:IsType(TYPE_TRAP+TYPE_COUNTER)) and c:IsControler(tp) and not c:IsPublic()
-		and c:IsPreviousLocation(LOCATION_DECK) and c:GetPreviousControler()==tp
+    return c:IsType(TYPE_TRAP) and (c:IsType(TYPE_CONTINUOUS) or c:IsType(TYPE_COUNTER)) and c:IsControler(tp) and not c:IsPublic()
+        and c:IsPreviousLocation(LOCATION_DECK) and c:GetPreviousControler()==tp
 end
 function c12010037.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and eg:IsExists(c12010037.filter,1,nil,tp) end
-	local g=eg:Filter(c12010037.filter,nil,tp)
-	if g:GetCount()==1 then
-		Duel.ConfirmCards(1-tp,g)
-		Duel.ShuffleHand(tp)
-		Duel.SetTargetCard(g)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.ConfirmCards(1-tp,sg)
-		Duel.ShuffleHand(tp)
-		Duel.SetTargetCard(sg)
-	end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and eg:IsExists(c12010037.filter,1,nil,tp) end
+    local g=eg:Filter(c12010037.filter,nil,tp)
+    if g:GetCount()==1 then
+        Duel.ConfirmCards(1-tp,g)
+        Duel.ShuffleHand(tp)
+        Duel.SetTargetCard(g)
+    else
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.ConfirmCards(1-tp,sg)
+        Duel.ShuffleHand(tp)
+        Duel.SetTargetCard(sg)
+    end
 end
 function c12010037.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SSet(tp,tc)
-		Duel.ConfirmCards(1-tp,tc)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e1)
-	end
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.SSet(tp,tc)
+        Duel.ConfirmCards(1-tp,tc)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+        e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+        e1:SetReset(RESET_EVENT+0x1fe0000)
+        tc:RegisterEffect(e1)
+    end
 end
 function c12010037.filter1(c,tp)
-	return c:IsType(TYPE_SPELL+TYPE_CONTINUOUS) and c:IsSetCard(0xfbc) and c:IsControler(tp) and not c:IsPublic()
-		and c:IsPreviousLocation(LOCATION_DECK) and c:GetPreviousControler()==tp
+    return c:IsType(TYPE_SPELL) and c:IsType(TYPE_CONTINUOUS+TYPE_FIELD) and c:IsSetCard(0xfbc) and c:IsControler(tp) and not c:IsPublic()
+        and c:IsPreviousLocation(LOCATION_DECK) and c:GetPreviousControler()==tp
 end
 function c12010037.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and eg:IsExists(c12010037.filter1,1,nil,tp) end
-	local g=eg:Filter(c12010037.filter1,nil,tp)
-	if g:GetCount()==1 then
-		Duel.ConfirmCards(1-tp,g)
-		Duel.ShuffleHand(tp)
-		Duel.SetTargetCard(g)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.ConfirmCards(1-tp,sg)
-		Duel.ShuffleHand(tp)
-		Duel.SetTargetCard(sg)
-	end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and eg:IsExists(c12010037.filter1,1,nil,tp) end
+    local g=eg:Filter(c12010037.filter1,nil,tp)
+    if g:GetCount()==1 then
+        Duel.ConfirmCards(1-tp,g)
+        Duel.ShuffleHand(tp)
+        Duel.SetTargetCard(g)
+    else
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.ConfirmCards(1-tp,sg)
+        Duel.ShuffleHand(tp)
+        Duel.SetTargetCard(sg)
+    end
 end
 function c12010037.spop1(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		Duel.RaiseEvent(tc,EVENT_CHAIN_SOLVED,tc:GetActivateEffect(),0,tp,tp,Duel.GetCurrentChain())
-	end
+    if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+        Duel.RaiseEvent(tc,EVENT_CHAIN_SOLVED,tc:GetActivateEffect(),0,tp,tp,Duel.GetCurrentChain())
+    end
 end
+
+
+
 
 
