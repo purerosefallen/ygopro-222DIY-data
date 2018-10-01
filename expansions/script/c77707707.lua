6d21e007f9553d8598f2f014dcb4d1a6e16af722
purerosefallen <78877@qq.com>
2018-10-01 13:33:35 +0000

    Auto Push by Sekka

--- expansions/script/c77707707.lua
+++ expansions/script/c77707707.lua
@@ -1,7 +1,7 @@
 --Vessel of Sin »Æ½ðÔ¿³×
 function c77707707.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -17,18 +17,53 @@ function c77707707.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
 function c77707707.filter(c)
-	return c:IsSetCard(0xa9c0) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsAbleToRemove() and c:IsFaceup()
 end
 
 function c77707707.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c77707707.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(c77707707.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g={}
+	for p=0,1 do
+		local mg=Duel.GetMatchingGroup(c77707707.filter,p,LOCATION_MZONE,0,nil)
+		if #mg>0 then
+			g[p]=mg:GetMaxGroup(Card.GetAttack)
+		else
+			g[p]=Group.CreateGroup()
+		end
+	end
+	local g_=g[0]:Clone()
+	g_:Merge(g[1])
+	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<=1 then
+		local rg=Duel.GetDecktopGroup(tp,40)
+		g_:Merge(rg)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g_,#g_,0,0)
+
 end
 function c77707707.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c77707707.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	local rg=Group.CreateGroup()
+	for p=0,1 do
+		local mg=Duel.GetMatchingGroup(c77707707.filter,p,LOCATION_MZONE,0,nil)
+		if #mg>0 then
+			g[p]=mg:GetMaxGroup(Card.GetAttack)
+		else
+			g[p]=Group.CreateGroup()
+		end
+		if #g[p]>1 then
+			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_REMOVE)
+			local sg=g[p]:Select(p,1,1,nil)
+			rg:Merge(sg)
+		else
+			rg:Merge(g[p])
+		end
+	end
+	if #rg>0 then
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+		if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)<=0 then
+			Duel.BreakEffect()
+			local dg=Duel.GetDecktopGroup(tp,40)
+			Duel.DisableShuffleCheck()
+			Duel.Remove(dg,POS_FACEDOWN,REASON_EFFECT)
+		end
 	end
 end
