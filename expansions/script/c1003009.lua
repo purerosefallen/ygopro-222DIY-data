c6ba994ac70e1f0a8438b116845c7f501f955379
篁楠 <HuangYuNan@users.noreply.github.com>
2018-12-03 19:55:23 +0800

    Update c1003009.lua

--- expansions/script/c1003009.lua
+++ expansions/script/c1003009.lua
@@ -106,7 +106,7 @@ end
 function c1003009.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRace(RACE_ZOMBIE) and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
