1fe7f5c2c2237b565ea076d928aef9f679a65628
purerosefallen <78877@qq.com>
2018-12-26 18:37:29 +0800

    filelist

--- update/update-push.sh
+++ update/update-push.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-sed -i "s/\r//g" ../expansions/script/*.lua
-sed -i "s/\r//g" ../expansions/*.conf
-sed -i "s/\r//g" ../deck/*.ydk
-rm -rf ../expansions/script/sed*
-rm -rf ../expansions/sed*
-rm -rf ../deck/sed*
-mono update.exe -ci
+./update-server.sh
 git add ../. -A
 git commit -m "update data"
 git push origin master
--- update/update-server.sh
+++ update/update-server.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+sed -i "s/\r//g" ../expansions/script/*.lua
+sed -i "s/\r//g" ../expansions/*.conf
+sed -i "s/\r//g" ../deck/*.ydk
+rm -rf ../expansions/script/sed*
+rm -rf ../expansions/sed*
+rm -rf ../deck/sed*
 mono update.exe -ci
