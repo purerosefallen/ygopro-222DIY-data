b5050a5a36ac3487cf72fda875fee364f6bbd8ec
purerosefallen <78877@qq.com>
2019-01-22 20:20:25 +0800

    fix

--- expansions/script/c77709001.lua
+++ expansions/script/c77709001.lua
@@ -26,7 +26,7 @@ function cm.initial_effect(c)
         if ct==3 then
             Duel.BreakEffect()
             if Duel.Draw(tp,2,REASON_EFFECT)>1 then
-                Duel.ShuffleHand(p)
+                Duel.ShuffleHand(tp)
                 Duel.BreakEffect()
                 Duel.DiscardHand(tp,nil,2,2,REASON_EFFECT+REASON_DISCARD)
             end
