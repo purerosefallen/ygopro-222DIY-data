1ba49a3501ebebae55b4efe01017f1ae1674f6f6
gg123gg <32418512+gg123gg@users.noreply.github.com>
2019-02-03 12:02:48 +0800

    Add files via upload

--- expansions/script/c12031011.lua
+++ expansions/script/c12031011.lua
@@ -9,6 +9,7 @@ function c12031011.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAIN_SOLVING)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c12031011.discon)
 	e1:SetOperation(c12031011.disop)
 	c:RegisterEffect(e1)
 
@@ -35,7 +36,7 @@ function c12031011.cfilter(c,tp)
 	return c:IsType(TYPE_MONSTER)
 end
 function c12031011.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if not eg and re and re:IsHasType(0x7e0) and re:GetHandler():IsReason(REASON_COST) then return end
+	if not eg and re and re:IsHasType(0x7e0) and re:GetHandler():IsReason(REASON_COST) and c: then return end
 	local sg=eg:Filter(c12031011.cfilter,nil,tp)
 	local tc=sg:GetFirst()
 	while tc do
@@ -46,15 +47,26 @@ end
 function c12031011.ovfilter(c)
 	return c:IsFaceup() and c:IsCode(12031000)
 end
+function c12031011.discon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp
+end
 function c12031011.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not Duel.GetFlagEffect(tp,12031011)==0 and rp==1-tp then return end
-	if not re:GetHandler():IsRelateToEffect(e)  then
-	Duel.NegateEffect(ev)
-	else
+	local tt=c:GetOverlayCount()
+	if Duel.GetFlagEffect(tp,12031011)==0 then 
+	if not ( re:IsHasType(EFFECT_TYPE_ACTIVATE) and not re:GetHandler():IsType(TYPE_CONTINUOUS) )  then
 	Duel.Overlay(c,Group.FromCards(re:GetHandler()))
-	Duel.RegisterFlagEffect(tp,12031011,RESET_PHASE+PHASE_END,0,1)
-	c:RegisterFlagEffect(12031011,RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(12031011,4))
+	   Duel.BreakEffect()
+	   local ff=c:GetOverlayCount()
+	   if ff>tt then
+		Duel.RegisterFlagEffect(tp,12031011,RESET_PHASE+PHASE_END,0,1)
+		c:RegisterFlagEffect(12031011,RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(12031011,4))
+	   else
+		  Duel.NegateEffect(ev)
+	   end
+	else
+		Duel.NegateEffect(ev)
+	end
 	end
 end
 function c12031011.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -76,13 +88,17 @@ function c12031011.thop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,12031011+100)==0 or Duel.IsExistingMatchingCard(c12031011.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_GRAVE+LOCATION_REMOVED,cc,nil) then return end
 	if cc<3 then
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	Duel.SelectMatchingCard(tp,c12031011.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_GRAVE+LOCATION_REMOVED,cc,cc,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	Duel.DiscardHand(tp,nil,cc-1,cc-1,nil)
+	local ct=Duel.SelectMatchingCard(tp,c12031011.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_GRAVE+LOCATION_REMOVED,cc,cc,nil)
+	   if Duel.SendtoHand(ct,tp,REASON_EFFECT)>0 then
+	   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	   Duel.DiscardHand(tp,nil,cc-1,cc-1,nil)
+	   end
 	else
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	Duel.SelectMatchingCard(tp,c12031011.thfilter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_GRAVE+LOCATION_REMOVED,cc,cc,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	Duel.DiscardHand(tp,nil,cc-1,cc-1,nil)
+	local ct=Duel.SelectMatchingCard(tp,c12031011.thfilter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_GRAVE+LOCATION_REMOVED,cc,cc,nil)
+	   if Duel.SendtoHand(ct,tp,REASON_EFFECT)>0 then
+	   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	   Duel.DiscardHand(tp,nil,cc-1,cc-1,nil)
+	   end
 	end
 end
