be087313cf73b460b4eea13589d7201edf3a2ba1
purerosefallen <78877@qq.com>
2019-02-10 22:31:21 +0800

    fix

--- expansions/script/c77709005.lua
+++ expansions/script/c77709005.lua
@@ -5,7 +5,8 @@ xpcall(function() require("expansions/script/c37564765") end,function() require(
 cm.Sekka_name_with_lap=true
 function cm.initial_effect(c)
 	c:EnableReviveLimit()
-	local function con(e,tp,eg,ep,ev,re,r,rp,chk)
+	local function con(e)
+        local tp=e:GetHandlerPlayer()
 		return Duel.GetCurrentPhase()>PHASE_DRAW and Duel.GetTurnPlayer()~=tp
 	end
 	local e1=Effect.CreateEffect(c)
