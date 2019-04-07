34837550d69597f7d7eafc4272c7025b6c3f7e89
purerosefallen <78877@qq.com>
2019-04-07 16:11:41 +0800

    remove

--- expansions/script/c33310023.lua
+++
@@ -1,6 +0,0 @@
---泰拉居民 向导
-local m=33310023
-local cm=_G["c"..m]
-if cm then
-   cm.setcard="terraria"
-end
--- expansions/script/c33310024.lua
+++
@@ -1,9 +0,0 @@
---泰拉魔物 金鱼
-if not pcall(function() require("expansions/script/c33310023") end) then require("script/c33310023") end
-local m=33310024
-local cm=_G["c"..m]
-function cm.initial_effect(c)
-	--pendulum summon
-	aux.EnablePendulumAttribute(c)  
-end
-cm.setcard="terraria"
