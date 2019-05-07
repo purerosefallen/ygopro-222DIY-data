604ba4503b0904d633a07c705764bda0019a380f
purerosefallen <78877@qq.com>
2019-05-07 12:53:08 +0800

    fix

--- expansions/script/c37564317.lua
+++ expansions/script/c37564317.lua
@@ -4,13 +4,7 @@ local cm=_G["c"..m]
 xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
 function cm.initial_effect(c)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(cm.target)
-	e1:SetOperation(cm.activate)
-	c:RegisterEffect(e1)
+	Auxiliary.AddRitualProcEqual2(c)
 	Senya.enable_get_all_cards()
 	--[[if not cm.gg then
 		cm.gg=Group.CreateGroup()
@@ -37,45 +31,4 @@ function cm.initial_effect(c)
 end
 function cm.ctfilter(c,code)
 	return c:GetOriginalCode()==code
-end
-function cm.filter(c,e,tp,m,ft)
-	if (c:GetType() & 0x81)~=0x81
-		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	if c.ritual_custom_condition then return c:ritual_custom_condition(mg,ft) end
-	if c.mat_filter then
-		mg=mg:Filter(c.mat_filter,nil)
-	end
-	return Senya.CheckRitualMaterial(c,mg,tp,c:GetLevel())
-end
-function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local mg=Duel.GetRitualMaterial(tp)
-		local ft=Duel.GetMZoneCount(tp)
-		return Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
-function cm.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetRitualMaterial(tp)
-	local ft=Duel.GetMZoneCount(tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft)
-	local tc=tg:GetFirst()
-	if tc then
-		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
-		if tc.ritual_custom_operation then
-			tc:ritual_custom_operation(mg)
-			local mat=tc:GetMaterial()
-			Duel.ReleaseRitualMaterial(mat)
-		else
-			if tc.mat_filter then
-				mg=mg:Filter(tc.mat_filter,nil)
-			end
-			local mat=Senya.CheckRitualMaterial(tc,mg,tp,tc:GetLevel())
-		end
-		Duel.BreakEffect()
-		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-		tc:CompleteProcedure()
-	end
 end
