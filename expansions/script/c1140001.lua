a39d91050ec963cc3a23c342e5567fe3356053ce
2924713558 <48914324+2924713558@users.noreply.github.com>
2019-06-05 09:26:13 +0800

    Add files via upload

--- expansions/script/c1140001.lua
+++ expansions/script/c1140001.lua
@@ -8,7 +8,6 @@ function c1140001.initial_effect(c)
 --
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(1140001,0))
-	e1:SetCategory(CATEGORY_SEARCH)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
